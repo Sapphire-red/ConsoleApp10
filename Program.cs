dccc2bc68c43965f778284e6fdeb3eeef32ee0ed
deltaDelete <samansy@yandex.ru>
2023-03-15 08:59:32 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine();
+        }
+    }
+    public class Monster {
+        public string Name { get; set; } = string.Empty;
+        public int Hearth {
+            get { return _hearth; }
+            set { _hearth = value; }
+        }
+        private int _hearth;
+        public int Strong {
+            get { return _strong; }
+            set { _strong = value; }
+        }
+        private int _strong;
+    }
+}
